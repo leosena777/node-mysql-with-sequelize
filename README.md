8fda70f33026419f87280267dfe66018a6fcae22
Leandro <leo.sena777@gmail.com>
2021-01-28 02:26:50 -0300

    adds sequelize + first migration

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # node-mysql-with-sequelize
 Server in node using mysql with sequelize
+
+
+# Database
+
+##  Migrations folder
+
+  /src/database/migrations
+
+##  command create data base
+
+npm run sequelize -- db:migrate
---
+++ src/database/index.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+const dbConfig = require("../config/database");
+
+const connection = new sequelize(dbConfig);
+
+module.exports = connection;
